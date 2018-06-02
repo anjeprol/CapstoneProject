fb543e32fa1b42e57c47a3747622e894605d37e5
Bharadwaj <vnaga810@gmail.com>
2018-06-02 16:17:26 -0400

    Added Navigation Drawer template code and integrated blank Fragment as first step

--- app/src/main/java/com/bharadwaj/android/capstoneproject/MapActivity.java
+++ app/src/main/java/com/bharadwaj/android/capstoneproject/MapActivity.java
@@ -2,12 +2,17 @@ package com.bharadwaj.android.capstoneproject;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
+import butterknife.BindView;
 import timber.log.Timber;
 import timber.log.Timber.DebugTree;
 
 
 public class MapActivity extends AppCompatActivity {
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
 
 
     @Override
@@ -17,13 +22,13 @@ public class MapActivity extends AppCompatActivity {
         Timber.plant(new DebugTree());
         Timber.d("Entering onCreate");
 
+        setSupportActionBar(toolbar);
+
         MapFragment mapFragment = new MapFragment();
         getSupportFragmentManager().beginTransaction().replace(R.id.map_fragment_placeholder, mapFragment).commit();
 
         Timber.d("Leaving onCreate");
     }
-
-
 
 
 }
